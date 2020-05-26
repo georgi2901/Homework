a6fd27e771dde445f3704f7f67ed0109a9191364
georgi2901 <65902806+georgi2901@users.noreply.github.com>
2020-05-26 11:49:06 +0300

    Add files via upload

---
+++ Eligible/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eligible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> studentsDictionary = new Dictionary<string, string>();
+            List<Student> students = new List<Student>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "end") { break; }
+
+                string[] data = input.Split(new string[] { "=>" }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+                double mark = Convert.ToDouble(data[3]);
+
+                Student s = new Student(data[1], data[0], data[2], mark);
+
+                try
+                {
+                    studentsDictionary.Add(s.Fn, s.Discipline);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                students.Add(s);
+            }
+
+            foreach (var student in students.OrderByDescending(x => x.Mark))
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
---
+++ Eligible/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eligible
+{
+    class Student
+    {
+        public string Discipline { get; set; }
+        public string Name { get; set; }
+        public string Fn { get; set; }
+        public double Mark { get; set; }
+
+        public Student(string name, string discipline, string fn, double mark)
+        {
+            Discipline = discipline;
+            Name = name;
+            Fn = fn;
+            Mark = mark;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Discipline + " " + Mark;
+        }
+    }
+}
